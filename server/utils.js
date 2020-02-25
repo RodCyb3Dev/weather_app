813c1a1d21013903dde8a99125b0d1cac32e3b09
Rodney H <rodney.hammad@gmail.com>
2020-02-25 22:33:13 +0200

    updated dockerfiles

--- server/utils.js
+++ server/utils.js
@@ -1,6 +1,9 @@
 const url = require('url');
 const config = require('./config');
 
+const five_day_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=';
+const full_five_URL = five_day_URL + '4c747836ffcebf6567e029f7a61a0ef0';
+
 module.exports = {
   generateWebAppURL: function(locationConfigType, locationConfigData) {
     const baseUrlConfig = config.baseUrl;
@@ -28,4 +31,28 @@ module.exports = {
       query: requestQuery,
     });
   },
+  // GET 5 DAY FORECAST
+	getFiveDayTemp: function(location) {
+		//web safe encoding
+		var encodedLocation = encodeURIComponent(location);
+		//interpolate string
+		var requestUrl = `${full_five_URL}&q=${encodedLocation}`;
+		console.log(requestUrl);
+
+		//========================================
+		// promise callback funcs
+		function success(res) {
+			if(res.status === 200) {
+				return res.data;
+			} else {
+				throw new Error(res.data.message);
+			}
+		};
+
+		function fail(err){
+			throw new Error(err.response.data.message);
+		}
+
+		return axios.get(requestUrl).then(success, fail);
+	}
 };
